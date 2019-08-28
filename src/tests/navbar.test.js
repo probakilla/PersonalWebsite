1123ac98a4289916f19417d5fec0ecd61e117e9c
Julien <probakilla@gmail.com>
2019-08-28 10:13:03 +0200

    Adding a render test for the navbar componenet

--- src/tests/navbar.test.js
+++ src/tests/navbar.test.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SiteNavbar from '../components/Navbar';
+import { render } from '@testing-library/react';
 
 describe('Navbar tests', () => {
     it('Render without crashing', () => {
         const div = document.createElement('div');
         ReactDOM.render(<SiteNavbar />, div);
+    });
+
+    it('Testing home button rendering', () => {
+        const { getByText } = render(<SiteNavbar />);
+        expect(getByText('Home')).toBeInTheDocument();
+    });
+
+    it('Testing about rendering', () => {
+        const { getByText } = render(<SiteNavbar />);
+        expect(getByText('About')).toBeInTheDocument();
+    });
+
+    it('Testing resume button rendering', () => {
+        const { getByText } = render(<SiteNavbar />);
+        expect(getByText('Resume')).toBeInTheDocument();
+    });
+
+    it('Testing contact button rendering', () => {
+        const { getByText } = render(<SiteNavbar />);
+        expect(getByText('Contact')).toBeInTheDocument();
     });
 });
