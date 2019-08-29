18895677d0cf896bd791a20472695be029a9ba58
Julien <probakilla@gmail.com>
2019-08-29 11:02:22 +0200

    Adding a dropdown button for the personal projects

--- src/components/SiteNavbar.js
+++ src/components/SiteNavbar.js
@@ -1,14 +1,31 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import Dropdown from 'react-bootstrap/Dropdown';
+
 import '../css/navbar.css';
 
 class SiteNavbar extends React.Component {
     render() {
         return (
             <Navbar fixed='top'>
-                <Navbar.Brand href='/'>Home</Navbar.Brand>
-                <Nav className='ml-auto'>
+                <Navbar.Brand className='mr-auto nav-elements' href='/'>
+                    Home
+                </Navbar.Brand>
+                <Nav className='ml-auto nav-element'>
+                    <Dropdown>
+                        <Dropdown.Toggle className='dropdown' variant='link'>
+                            Projects
+                        </Dropdown.Toggle>
+                        <Dropdown.Menu className='dropdown-menu'>
+                            <Dropdown.Item
+                                className='dropwdown-item'
+                                href='https://probakilla.github.io/HeroesAndCreaturesOnline'
+                            >
+                                Heroes And Creatures Online
+                            </Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
                     <Nav.Link href='/resume'>Resume</Nav.Link>
                     <Nav.Link href='/contact'>Contact</Nav.Link>
                 </Nav>
